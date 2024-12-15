66f52141ea53c87106abef427d74a24b8a56158d
HarrietNditii <harrietnditiwanjohi@gmail.com>
2024-12-15 13:58:43 +0300

    Initial commit: Added submission form project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SubmissionForm from './SubmissionForm';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React Submission Form</h1>
+            <SubmissionForm />
+        </div>
+    );
+};
+
+export default App;
